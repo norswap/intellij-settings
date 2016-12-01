073f651226203eb56511e1145e9fa7ce3a05f510
Nicolas Laurent <norswap@gmail.com>
2016-12-01 13:58:16 +0100

    fix win to mac conversion script

--- kbd_win_to_mac.sh
+++ kbd_win_to_mac.sh
@@ -5,5 +5,5 @@ DIR="$(cd "$(dirname "$SOURCE")" && pwd)"
 
 cd $DIR/keymaps
 sed 's/ctrl /meta /g' norswap_win.xml > norswap_mac.xml.tmp
-awk '!x{x=sub(/norswap_mac/,"norswap_win")}1' norswap_mac.xml.tmp > norswap_mac.xml
+awk '!x{x=sub(/norswap_win/,"norswap_mac")}1' norswap_mac.xml.tmp > norswap_mac.xml
 rm norswap_mac.xml.tmp
